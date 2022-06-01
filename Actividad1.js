ffe29a82a3a08d0d93cab0a3a1cc5d6f7777c5ea
Alberto-SSanchez <65683678+Alberto-SSanchez@users.noreply.github.com>
2022-05-31 19:59:30 -0500

    Add files via upload
    Actividad1

---
+++ Actividad1.js
@@ -0,0 +1,19 @@
+// Multiply
+var inputNumber = prompt("Enter a number to multiply it by two");
+var numberByTwo = inputNumber * 2;
+console.log("Result is ->", numberByTwo)
+
+//Substract
+var numberOne = prompt("Enter a number");
+var numberTwo = prompt("Enter another number");
+console.log("The result of substracting the second number is ->", numberOne - numberTwo);
+
+//Circle area
+var radius = 5.3;
+var pi = Math.PI;
+console.log("Area of circle of radius 5.3 is ->", pi*(radius**2))
+
+//Rhombus
+var d = 6;
+var D = 8;
+console.log("Area of a rhombus of d=6cm and D=8cm is ->", (d*D)/2)
