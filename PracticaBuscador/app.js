25c56f1b44636f50ba2c4fdee444a8b8fa747a9c
alberto sanchez <soy_uno_2008@htmail.com>
2022-06-20 16:51:43 -0500

    Uploading Practica Buscador

---
+++ PracticaBuscador/app.js
@@ -0,0 +1,100 @@
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-field');
+const gridMovies = document.querySelector('#grid-movies');
+const buttons = document.querySelector('#category-buttons');
+
+let searchTerm = '';
+
+function displayCryptos (cryptocurrencies){
+
+    clearCryptos();
+
+    cryptocurrencies.forEach( currency => {
+
+        const {crypto, price, marketCap, website, category, image} = currency;
+
+        const cardCryptocurrency = document.createElement('div');
+        cardCryptocurrency.classList.add('col');
+        cardCryptocurrency.innerHTML = `
+        <div class="card h-100">
+            <div class="card-body">
+                <img src="${image}" class="img-fluid">
+                <p class="card-text mb-1">Price: ${price}</p>
+                <p class="card-text mb-1">Marketcap: ${marketCap}</p>
+                <p class="card-text mb-1">Website: <a href="${website}" target="_blank">${website}</a></p>
+        
+            </div>
+        </div>
+        `
+
+        gridMovies.appendChild(cardCryptocurrency);
+
+    } )
+}
+
+function clearCryptos(){
+    gridMovies.innerHTML = '';
+    while(gridMovies.firstChild){gridMovies.firstChild.remove();}
+}
+
+function filterCryptos(currency){
+    if (searchTerm){
+        if(currency.crypto.toLowerCase()==searchTerm){
+            return currency;
+        }
+    }
+}
+
+function filterCategory(currency){
+    
+    if(searchTerm != 'category'){
+        if(currency.category.includes(searchTerm)){
+            return currency;
+        }
+    }
+}
+
+function displayNoResult(){
+    clearCryptos();
+ 
+    const noResult = document.createElement('div');
+    noResult.classList.add('alert', 'alert-danger', 'w-100', 'text-center');
+    noResult.textContent = 'No hay resultados de busqueda';
+
+    if(gridMovies.parentElement.children.length <2){
+        gridMovies.appendChild(noResult);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayCryptos(cryptocurrencies);
+});
+
+searchForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    searchTerm = searchInput.value.toLowerCase();
+    
+    result = cryptocurrencies.filter(filterCryptos);
+
+    if(result.length > 0){
+        displayCryptos(result);
+    } 
+    else{
+        displayNoResult();
+    }
+    
+    searchForm.reset();
+})
+
+buttons.addEventListener('click', (e) =>{
+
+    searchTerm = e.target.id;
+    result = cryptocurrencies.filter(filterCategory);
+
+    if(result.length > 0){
+        displayCryptos(result);
+    } 
+    else{
+        displayNoResult();
+    }
+})
---
+++ PracticaBuscador/db.js
@@ -0,0 +1,134 @@
+const cryptocurrencies = [{
+    crypto: 'Bitcoin',
+    price: '20,788.95 USD',
+    marketCap: '396,497,007,990 USD',
+    website: 'https://bitcoin.org/es/',
+    category: "bitcoin",
+    image: './img/bitcoin.png'
+},
+{
+    crypto: 'Ethereum',
+    price: '1,131.93 USD',
+    marketCap: '137,172,615,400 USD',
+    website: 'https://ethereum.org/en/',
+    category: 'ethereum',
+    image: './img/ether.png'
+},
+{
+    crypto: 'Tether',
+    price: '1 USD',
+    marketCap: '68,066,678,238 USD',
+    website: 'https://tether.to/es/',
+    category: 'ethereum stable',
+    image: './img/usdt.png'
+
+},
+{
+    crypto: 'USD Coin',
+    price: '1 USD',
+    marketCap: '55,979,473,855 USD',
+    website: 'https://www.circle.com/en/usdc',
+    category: 'ethereum stable',
+    image: './img/usdc.png'
+
+},
+{
+    crypto: 'Binance USD',
+    price: '1 USD',
+    marketCap: '17,287,786,062 USD',
+    website: 'https://paxos.com/busd/',
+    category: 'stable',
+    image: './img/busd.png'
+
+},
+{
+    crypto: 'Cardano',
+    price: '0.5053 USD',
+    marketCap: '16,746,628,066 USD',
+    website: 'https://cardano.org/',
+    category: 'other',
+    image: './img/ada.png'
+
+},
+{
+    crypto: 'Solana',
+    price: '36.56 USD',
+    marketCap: '11,991,432,780 USD',
+    website: 'https://solana.com',
+    category: 'other',
+    image: './img/sol.png'
+
+},
+{
+    crypto: 'Polkadot',
+    price: '7.81 USD',
+    marketCap: '8,813,339,916 USD',
+    website: 'https://polkadot.network/',
+    category: 'other',
+    image: './img/dot.png'
+
+},
+{
+    crypto: 'Dogecoin',
+    price: '0.0597 USD',
+    marketCap: '7,931,786,372 USD',
+    website: 'https://dogecoin.com/',
+    category: 'other',
+    image: './img/doge.png'
+
+},
+{
+    crypto: 'Shiba Inu',
+    price: '0.00000812 USD',
+    marketCap: '4,777,838,180 USD',
+    website: 'https://shibatoken.com/',
+    category: 'other',
+    image: './img/shiba.png'
+
+},
+{
+    crypto: 'Decentraland',
+    price: '0.8206 USD',
+    marketCap: '1,230,025,958 USD',
+    website: 'https://decentraland.org/',
+    category: 'metaverse ethereum',
+    image: './img/mana.png'
+
+},
+{
+    crypto: 'Sandbox',
+    price: '0.8541 USD',
+    marketCap: '1,075,169,071 USD',
+    website: 'https://www.sandbox.game/en/',
+    category: 'metaverse ethereum',
+    image: './img/sand.png'
+
+},
+{
+    crypto: 'Polygon',
+    price: '0.3932 USD',
+    marketCap: '2,715,928,107 USD',
+    website: 'https://polygon.technology',
+    category: 'other ethereum',
+    image: './img/matic.png'
+
+},
+{
+    crypto: 'Monero',
+    price: '114.43 USD',
+    marketCap: '2,081,922,290 USD',
+    website: 'https://www.getmonero.org',
+    category: 'other',
+    image: './img/monero.png'
+
+},
+{
+    crypto: 'Filecoin',
+    price: '5.37 USD',
+    marketCap: '1,197,490,892 USD',
+    website: 'https://filecoin.io/',
+    category: 'other',
+    image: './img/file.png'
+
+},
+];
